1b668fa43dee05186d1fa2d7839544094eca7b14
lin00 <you@example.com>
2022-03-16 22:15:10 +0800

    提添加指定ip启动

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/server"
 	"log"
+	"net"
 	"user/kitex_gen/user/user"
 )
 
 func main() {
-	svr := user.NewServer(new(UserImpl))
+	ipAddr, err := net.ResolveIPAddr("tcp", "127.0.0.1:8000")
+	if err != nil {
+		panic("failed to lesson port")
+	}
+	svr := user.NewServer(new(UserImpl), server.WithServiceAddr(ipAddr))
 
-	err := svr.Run()
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
